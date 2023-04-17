0f88e56dee2e1a7246d52b7f634e38d9b7472f8b
matsueushi <matsueushi@gmail.com>
2023-04-17 14:01:57 +0000

    working...

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -1,5 +1,8 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 use proconio::{input, source::Source};
-use rand::{Rng, SeedableRng};
+// use rand::{Rng, SeedableRng};
 use std::io::BufRead;
 
 const D_MAX: usize = 10;
@@ -28,13 +31,26 @@ impl Input {
     }
 }
 
+impl std::fmt::Display for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.n, self.k)?;
+        write!(
+            f,
+            "{}",
+            self.a
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(" ")
+        )?;
+        for &(x, y) in &self.xy {
+            write!(f, "{} {}", x, y)?;
+        }
+        Ok(())
+    }
+}
+
 // ロジック
-
-// 座標圧縮
-// pub fn compress_coord(coords: &[(i64, i64)]) {
-//     eprintln!("{:?}", coords);
-// }
-
 type Line = (i64, i64, i64, i64);
 
 // カットした情報の出力用
@@ -91,12 +107,24 @@ impl Cut {
         }
         cut_lines
     }
+
+    // スコアを計算したい
+    fn score(&self, cake: &Cake) {
+        let ss = vec![0; 10];
+        for u in 0..self.us.len() {
+            let x = cake.xs[u];
+            for v in 0..self.vs.len() {
+                let y = cake.ys[v];
+            }
+        }
+    }
 }
 
 // 座標圧縮して使いやすい状態になっているケーキ
 struct Cake {
-    xs: Vec<i64>, // 圧縮後のx座標
-    ys: Vec<i64>, // 圧縮後のy座標
+    xs: Vec<i64>,        // 圧縮後のx座標
+    ys: Vec<i64>,        // 圧縮後のy座標
+    cs: Vec<Vec<usize>>, // 累積されている数
 }
 
 impl Cake {
@@ -107,30 +135,78 @@ impl Cake {
             xs.push(x);
             ys.push(y);
         }
-        Self { xs, ys }
+        xs.sort();
+        ys.sort();
+        xs.dedup();
+        ys.dedup();
+
+        let n = xs.len();
+        let m = ys.len();
+        let mut zs = vec![vec![0; m]; n];
+        for (x, y) in &input.xy {
+            let i = xs.binary_search(x).unwrap();
+            let j = ys.binary_search(y).unwrap();
+            zs[i][j] += 1;
+        }
+
+        let mut cs = vec![vec![0; m + 1]; n + 1];
+        for i in 0..n {
+            for j in 0..m {
+                cs[i + 1][j + 1] = cs[i + 1][j] + zs[i][j];
+            }
+        }
+        for j in 0..m {
+            for i in 0..n {
+                cs[i + 1][j + 1] += cs[i][j + 1];
+            }
+        }
+
+        Self { xs, ys, cs }
     }
 }
 
 pub fn solve(input: &Input) {
     let cake = Cake::new(&input);
-
-    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
+    // eprintln!("{}", cake.xs.len());
+    // eprintln!("{}", cake.ys.len());
+    // let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
 
     let k = input.k;
     let step_x = 2 * cake.xs.len() / k;
     let step_y = 2 * cake.ys.len() / k;
-    for _ in 0..50 {
-        let mut us = Vec::new();
-        let mut vs = Vec::new();
-        for u in (0..cake.xs.len() - step_x).step_by(step_x) {
-            let r = rng.gen_range(0, step_x / 2);
-            us.push(u + r);
-        }
-        for v in (0..cake.ys.len() - step_y).step_by(step_y) {
-            let r = rng.gen_range(0, step_y / 2);
-            vs.push(v + r);
-        }
-        let cut = Cut { us, vs };
-        println!("{}", cut.lines(&cake));
+    let mut us = Vec::new();
+    let mut vs = Vec::new();
+    for u in (step_x..cake.xs.len()).step_by(step_x) {
+        us.push(u);
+    }
+    for v in (step_y..cake.ys.len()).step_by(step_y) {
+        vs.push(v);
+    }
+    let cut = Cut { us, vs };
+    println!("{}", cut.lines(&cake));
+
+    cut.score(&cake);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cake() {
+        let input = Input {
+            n: 1 * 1 + 2 * 1 + 3 * 1,
+            k: 10,
+            a: vec![1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            xy: vec![
+                (100, 100),
+                (200, 200),
+                (100, 200),
+                (300, 300),
+                (400, 400),
+                (500, 500),
+            ],
+        };
+        println!("{}", input);
     }
 }
